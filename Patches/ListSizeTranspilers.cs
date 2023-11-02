b23bfa6d63debae73862d57dc47a3e4f41d68004
jayden <46307433+krmeet@users.noreply.github.com>
2023-11-02 20:59:45 +0000

    add another transpiler

--- Patches/ListSizeTranspilers.cs
+++ Patches/ListSizeTranspilers.cs
@@ -25,6 +25,32 @@ namespace BigLobby.Patches
             }
             return codes.AsEnumerable();
         }
+        [HarmonyPatch(typeof(StartOfRound), "SyncShipUnlockablesServerRpc")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> SyncUnlockablesRpc(IEnumerable<CodeInstruction> instructions) {
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Newarr)
+                {
+                    if (codes[i - 1].OperandIs(4)) {
+                        codes[i - 1].operand = 4;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Blt_S)
+                {
+                    if (codes[i - 1].OperandIs(4)) {
+                        codes[i - 1].operand = 4;
+                        break;
+                    }
+                }
+            }
+            return codes.AsEnumerable();
+        }
         [HarmonyPatch(typeof(PlayerControllerB), "SendNewPlayerValuesServerRpc")]
         [HarmonyTranspiler]
         public static IEnumerable<CodeInstruction> SendPlayerValuesRpc(IEnumerable<CodeInstruction> instructions) {
