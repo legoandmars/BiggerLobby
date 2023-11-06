d80962d43bae75782536d4c30a91590b8b159e40
bizzlemip <149446542+bizzlemip@users.noreply.github.com>
2023-11-06 12:56:03 -0500

    Update LobbySize.cs

--- Patches/LobbySize.cs
+++ Patches/LobbySize.cs
@@ -1,6 +1,8 @@
+using BepInEx;
 using HarmonyLib;
 using Steamworks;
 using Steamworks.Data;
+using System;
 using UnityEngine;
 
 namespace BigLobby.Patches
@@ -17,6 +19,19 @@ namespace BigLobby.Patches
         [HarmonyPrefix]
         public static bool SkipLobbySizeCheck(ref GameNetworkManager __instance, ref bool __result, Lobby lobby) {
             string data = lobby.GetData("vers");
+            string text = lobby.GetData("MaxPlayers");
+            int number;
+            if (!text.IsNullOrWhiteSpace() || !(int.TryParse(text, out number)))
+            {
+                number = 20;
+            }
+            number = Math.Min(Math.Max(number, 4),20);
+            if (lobby.MemberCount >= number || lobby.MemberCount < 1)
+            {
+                Debug.Log($"Lobby join denied! Too many members in lobby! {lobby.Id}");
+                UnityEngine.Object.FindObjectOfType<MenuManager>().SetLoadingScreen(isLoading: false, RoomEnter.Full, "The server is full!");
+                return false;
+            }
             if (data != __instance.gameVersionNum.ToString())
             {
                 Debug.Log($"Lobby join denied! Attempted to join vers.{data} lobby id: {lobby.Id}");
