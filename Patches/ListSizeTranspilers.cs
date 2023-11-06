544d2129c646006664939a4ae939a46f77482c58
bizzlemip <149446542+bizzlemip@users.noreply.github.com>
2023-11-06 12:55:48 -0500

    Update ListSizeTranspilers.cs

--- Patches/ListSizeTranspilers.cs
+++ Patches/ListSizeTranspilers.cs
@@ -4,21 +4,177 @@ using System.Linq;
 using System.Reflection.Emit;
 using GameNetcodeStuff;
 using HarmonyLib;
-
+using BepInEx;
+using UnityEngine;
 namespace BigLobby.Patches
 {
     [HarmonyPatch]
-    internal class ListSizeTranspilers {
+    public class ListSizeTranspilers {
         [HarmonyPatch(typeof(HUDManager), "SyncAllPlayerLevelsServerRpc")]
         [HarmonyTranspiler]
         public static IEnumerable<CodeInstruction> SyncLevelsRpc(IEnumerable<CodeInstruction> instructions) {
             var codes = new List<CodeInstruction>(instructions);
+            
             for (int i = 0; i < codes.Count; i++)
             {
                 if (codes[i].opcode == OpCodes.Newarr)
                 {
-                    if (codes[i - 1].OperandIs(4)) {
-                        codes[i - 1].operand = 4;
+                    if (codes[i - 1].opcode == OpCodes.Ldc_I4_4) {
+                        codes[i - 1].opcode = OpCodes.Ldc_I4_S;
+                        codes[i - 1].operand = Plugin.MaxPlayers;
+                    }
+                }
+            }
+            return codes.AsEnumerable();
+        }
+        [HarmonyPatch(typeof(CrawlerAI), "Start")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> CrawlerAIPatch(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Newarr)
+                {
+                    if (codes[i - 1].opcode == OpCodes.Ldc_I4_4)
+                    {
+                        codes[i - 1].opcode = OpCodes.Ldc_I4_S;
+                        codes[i - 1].operand = Plugin.MaxPlayers;
+                    }
+                }
+            }
+            return codes.AsEnumerable();
+        }
+        [HarmonyPatch(typeof(DressGirlAI), "ChoosePlayerToHaunt")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> DressPatch(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Newarr)
+                {
+                    if (codes[i - 1].opcode == OpCodes.Ldc_I4_4)
+                    {
+                        codes[i - 1].opcode = OpCodes.Ldc_I4_S;
+                        codes[i - 1].operand = Plugin.MaxPlayers;
+                    }
+                }
+            }
+            return codes.AsEnumerable();
+        }
+        [HarmonyPatch(typeof(PlayerControllerB), "SendNewPlayerValuesServerRpc")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> SendNewPlayerValuesServerRpc(IEnumerable<CodeInstruction> instructions) {
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Blt)
+                {
+                    if (codes[i-1].opcode == OpCodes.Ldc_I4_4)
+                    {
+                        codes[i - 1].opcode = OpCodes.Ldc_I4_S;
+                        codes[i - 1].operand = Plugin.MaxPlayers;
+                        break;
+                    }
+                }
+            }
+            return codes.AsEnumerable();
+        }
+        [HarmonyPatch(typeof(StartOfRound), "SyncShipUnlockablesClientRpc")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> SyncShipUnlockablesClientRpc(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Blt)
+                {
+                    if (codes[i - 1].opcode == OpCodes.Ldc_I4_4)
+                    {
+                        codes[i - 1].opcode = OpCodes.Ldc_I4_S;
+                        codes[i - 1].operand = Plugin.MaxPlayers;
+                        break;
+                    }
+                }
+            }
+            return codes.AsEnumerable();
+        }
+        [HarmonyPatch(typeof(DressGirlAI), "ChoosePlayerToHaunt")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> ChoosePlayerToHaunt(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Blt)
+                {
+                    if (codes[i - 1].opcode == OpCodes.Ldc_I4_4)
+                    {
+                        codes[i - 1].opcode = OpCodes.Ldc_I4_S;
+                        codes[i - 1].operand = Plugin.MaxPlayers;
+                        Debug.Log("Dress AI Fix Applied");
+                    }
+                }
+            }
+            return codes.AsEnumerable();
+        }
+        [HarmonyPatch(typeof(EnemyAI), "GetClosestPlayer")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> GetClosestPlayer(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Blt)
+                {
+                    if (codes[i - 1].opcode == OpCodes.Ldc_I4_4)
+                    {
+                        codes[i - 1].opcode = OpCodes.Ldc_I4_S;
+                        codes[i - 1].operand = Plugin.MaxPlayers;
+                        Debug.Log("Gen AI Fix Applied");
+                        break;
+                    }
+                }
+            }
+            return codes.AsEnumerable();
+        }
+        [HarmonyPatch(typeof(SpringManAI), "Update")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> SUpdate(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Blt)
+                {
+                    if (codes[i - 1].opcode == OpCodes.Ldc_I4_4 && codes[i - 2].opcode == OpCodes.Ldloc_2)
+                    {
+                        codes[i - 1].opcode = OpCodes.Ldc_I4_S;
+                        codes[i - 1].operand = Plugin.MaxPlayers;
+                        Debug.Log("Spring AI Fix 2 Applied");
+                        break;
+                    }
+                }
+            }
+            return codes.AsEnumerable();
+        }
+        [HarmonyPatch(typeof(SpringManAI), "DoAIInterval")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> AIInterval(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Blt)
+                {
+                    if (codes[i - 1].opcode == OpCodes.Ldc_I4_4)
+                    {
+                        codes[i - 1].opcode = OpCodes.Ldc_I4_S;
+                        codes[i - 1].operand = Plugin.MaxPlayers;
+                        Debug.Log("Spring AI Fix Applied");
                         break;
                     }
                 }
@@ -27,61 +183,52 @@ namespace BigLobby.Patches
         }
         [HarmonyPatch(typeof(StartOfRound), "SyncShipUnlockablesServerRpc")]
         [HarmonyTranspiler]
-        public static IEnumerable<CodeInstruction> SyncUnlockablesRpc(IEnumerable<CodeInstruction> instructions) {
+        public static IEnumerable<CodeInstruction> SyncShipUnlockablesServerRpc(IEnumerable<CodeInstruction> instructions)
+        {
             var codes = new List<CodeInstruction>(instructions);
             for (int i = 0; i < codes.Count; i++)
             {
-                if (codes[i].opcode == OpCodes.Newarr)
-                {
-                    if (codes[i - 1].OperandIs(4)) {
-                        codes[i - 1].operand = 4;
-                        break;
+                //if (codes[i].opcode == OpCodes.Blt)
+                //{
+                    if (codes[i].opcode == OpCodes.Ldc_I4_4)
+                    {
+                        codes[i].opcode = OpCodes.Ldc_I4_S;
+                        codes[i ].operand = Plugin.MaxPlayers;
                     }
-                }
-            }
-            for (int i = 0; i < codes.Count; i++)
-            {
-                if (codes[i].opcode == OpCodes.Blt_S)
-                {
-                    if (codes[i - 1].OperandIs(4)) {
-                        codes[i - 1].operand = 4;
-                        break;
-                    }
-                }
+                //}
             }
             return codes.AsEnumerable();
         }
-        [HarmonyPatch(typeof(PlayerControllerB), "SendNewPlayerValuesServerRpc")]
+        [HarmonyPatch(typeof(HUDManager), "FillEndGameStats")]
         [HarmonyTranspiler]
-        public static IEnumerable<CodeInstruction> SendPlayerValuesRpc(IEnumerable<CodeInstruction> instructions) {
+        public static IEnumerable<CodeInstruction> FillEndGameStats(IEnumerable<CodeInstruction> instructions)
+        {
             var codes = new List<CodeInstruction>(instructions);
             for (int i = 0; i < codes.Count; i++)
             {
-                if (codes[i].opcode == OpCodes.Blt_S)
+                if (codes[i].opcode == OpCodes.Blt)
                 {
-                    if (codes[i - 1].OperandIs(4)) {
-                        codes[i - 1].operand = 4;
-                        break;
-                    }
+                    codes[i].opcode = OpCodes.Bgt;
+                    //break;see if i care bozo ! ratio ecksdee blehj.
                 }
             }
             return codes.AsEnumerable();
         }
+
         [HarmonyPatch(typeof(PlayerControllerB), "SpectateNextPlayer")]
         [HarmonyTranspiler]
         public static IEnumerable<CodeInstruction> SpectateNextPlayer(IEnumerable<CodeInstruction> instructions) {
             var codes = new List<CodeInstruction>(instructions);
             for (int i = 0; i < codes.Count; i++)
             {
-                if (codes[i].opcode == OpCodes.Blt_S)
+                if (codes[i].opcode == OpCodes.Ldc_I4_4)
                 {
-                    if (codes[i - 1].OperandIs(4)) {
-                        codes[i - 1].operand = 4;
-                        break;
-                    }
+                    codes[i].opcode = OpCodes.Ldc_I4_S;
+                        codes[i].operand = Plugin.MaxPlayers;
                 }
             }
             return codes.AsEnumerable();
         }
+
     }
 }
