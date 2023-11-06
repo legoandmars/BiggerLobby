88a61714d9688ff7728c44ea7dcd357f52c75535
bizzlemip <149446542+bizzlemip@users.noreply.github.com>
2023-11-06 17:23:34 -0500

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,21 +1,31 @@
-﻿using BepInEx;
+using BepInEx;
 using HarmonyLib;
 using System.Reflection;
+using LC_API;
+using GameNetcodeStuff;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Unity.Netcode;
 
-namespace BigLobby
+namespace BiggerLobby 
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        public static int MaxPlayers = 8;
-
+        public static bool oldhastime;
+        public static int MaxPlayers = 20;
+        public static bool instantiating;
         private Harmony _harmony;
         private void Awake()
         {
             _harmony = new Harmony(PluginInfo.PLUGIN_GUID);
             _harmony.PatchAll();
-
             Logger.LogInfo($"{PluginInfo.PLUGIN_GUID} loaded");
+        }
+
+        private void OnDestroy()
+        {
+            LC_API.ServerAPI.ModdedServer.SetServerModdedOnly();
         }
     }
 }
