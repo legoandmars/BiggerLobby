2ad8de3193ad040674654d77cc805263611f56a9
bizzlemip <149446542+bizzlemip@users.noreply.github.com>
2023-11-11 19:14:15 -0500

    Update Helper.cs

--- Helper.cs
+++ Helper.cs
@@ -1,4 +1,7 @@
-namespace BigLobby
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BiggerLobby
 {
     public static class Helper
     {
@@ -6,6 +9,22 @@ namespace BigLobby
             var newArray = new T[newSize];
             oldArray.CopyTo(newArray, 0);
             return newArray;
+        }
+        public static void ResizeList<T>(this List<T> list, int size, T element = default(T))
+        {
+            int count = list.Count;
+
+            if (size < count)
+            {
+                list.RemoveRange(size, count - size);
+            }
+            else if (size > count)
+            {
+                if (size > list.Capacity)   // Optimization
+                    list.Capacity = size;
+
+                list.AddRange(Enumerable.Repeat(element, size - count));
+            }
         }
     }
 }
